8ae371a056e349e317ed4c91e526c7e71859983c
Ivan Mutavdzic <imutavdz@f0r4s8.codam.nl>
2024-10-30 18:39:12 +0100

    add

--- Libft/ft_putchar_fd.c
+++ Libft/ft_putchar_fd.c
@@ -1,37 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: imutavdz <imutavdz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/30 16:09:42 by imutavdz          #+#    #+#             */
-/*   Updated: 2024/10/30 18:38:46 by imutavdz         ###   ########.fr       */
+/*   Created: 2024/10/30 18:36:17 by imutavdz          #+#    #+#             */
+/*   Updated: 2024/10/30 18:38:08 by imutavdz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+void	ft_putchar_fd(char c, int fd)
 {
-	char	*dst;
-	size_t	i;
-	size_t	j;
-
-	i = 0;
-	j = 0;
-	dst = (char *)malloc(ft_strlen(s1) + (ft_strlen(s2) + 1) * sizeof(char));
-	if (!dst)
-		return (NULL);
-	while (s1[i] != '\0')
-	{
-		dst[i] = s1[i];
-		i++;
-	}
-	while (s2[j] != '\0')
-	{
-		dst[i + j] = s2[j];
-		j++;
-	}
-	dst[i + j] = '\0';
-	return (dst);
+	write (fd, &c, 1);
 }
